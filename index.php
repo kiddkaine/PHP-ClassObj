edf0aa4e8431accd749116defd1385c63d067f95
Kidd Kaine <79826408+kiddkaine@users.noreply.github.com>
2022-04-19 00:23:50 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Обучение PHP</title>
 	</head>
-	<body>
+	<body align="center">
 		<?php
 			class Users // Создаём класс
 			{
